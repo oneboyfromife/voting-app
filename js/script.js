1ceace279ac2429b1a19e2191bf0f18782729103
katiamiyu <katiamiyu@gmail.com>
2022-07-15 12:36:01 +0100

    add the vote progress chat

--- js/script.js
+++ js/script.js
@@ -2,16 +2,24 @@
 var vote_btn = document.querySelector(".submit-btn");
 var stateSelect = document.querySelectorAll("select")[0];
 var candidateSelect = document.querySelectorAll(".who-form-img");
-var progressBar = document.querySelector("progress")
+
 var spanElon = document.querySelector(".elon-vote-count");
 var spanEdward = document.querySelector(".edward-vote-count");
+
+// display the total vote
 var voteTotal = document.querySelector(".total-votes");
+
+// the progress bar chart of the candidates
+var elonChart = document.querySelector(".elon-progress");
+var edwardChart = document.querySelector(".edward-progress");
 
 
 // global variables
 let elonCont = 0;
 let edwardCount = 0;
+let totalvotes = 0;
 let candidate = "";
+
 
 // getting img id
 for(var x = 0; x<2; x++){
@@ -21,13 +29,20 @@ for(var x = 0; x<2; x++){
     })
 }
 
-function getTotalVotes(el, ed){
-    return el + ed;
+// calculate total votes of the contestants
+function updateVoteTotal(el, ed){
+    totalvotes = el + ed;
+    voteTotal.innerText = String(totalvotes) + " :vote(s)";
 }
 
-function calcProgress(elon, total){
-    result = Math.floor(elon/total*100)
-    return result;
+// refresh progresschart
+function refreshProgress(elon,edward, total){
+    let elonprogress = Math.floor(elon/total*100);
+    let edwardprogress = Math.floor(edward/total*100);
+
+    // update progress chart
+    elonChart.style.width = String(elonprogress)+"%";
+    edwardChart.style.width = String(edwardprogress) +"%";
 }
 
 
@@ -40,15 +55,17 @@ function voteEvent(e){
             alert("you have selected Elon");
             elonCont += 1
             spanElon.innerText = elonCont;
+            refreshProgress(elonCont, edwardCount, totalvotes)
+
         }else if(candidate === "edwardCount"){
             alert("you have selected Edward");
             edwardCount += 1;
             spanEdward.innerText = edwardCount;
+            refreshProgress(elonCont, edwardCount, totalvotes)
         }
-        let totalvotes = getTotalVotes(elonCont, edwardCount);
-        let progressCount = calcProgress(elonCont, totalvotes);
-        voteTotal.innerText = String(totalvotes) + " :vote(s)";
-        //progressBar.value = progressCount;
+
+        // update total votes label
+        updateVoteTotal(elonCont, edwardCount);        
     }
     else{
         alert("Please choose a candidate!")
